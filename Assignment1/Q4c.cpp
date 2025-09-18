f1de5f7d7c0e6642399a1fe407cc1ae28e932aa8
Arnav Gupta <arnavg2302@gmail.com>
2025-09-18 12:52:51 +0530

    Update Q4c.cpp

--- Assignment1/Q4c.cpp
+++ Assignment1/Q4c.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
     int rows, cols;
-    cout << "Enter rows and columns of matrix: ";
+    cout << "Enter rows and columns of matrix";
     cin >> rows >> cols;
 
     int mat[20][20], trans[20][20];
 
-    cout << "Enter elements of matrix:\n";
+    cout << "Enter elements of matrix";
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             cin >> mat[i][j];
@@ -21,7 +21,7 @@ int main() {
         }
     }
 
-    cout << "Transpose of matrix is:\n";
+    cout << "Transpose of matrix is";
     for (int i = 0; i < cols; i++) {
         for (int j = 0; j < rows; j++) {
             cout << trans[i][j] << " ";
@@ -29,4 +29,5 @@ int main() {
         cout << endl;
     }
 }
+
 
